34d828137d5390c8b9dbba52d3215dcd9ba9c965
Sheng <swang720@gmail.com>
2012-09-28 14:36:08 -0700

    Added leftpane.php, modified other php files to include this as a resource.

---
+++ leftpane.php
@@ -0,0 +1,19 @@
+<div id="leftpane">
+	<div id="sidemenu">
+		<a href="index.php"><img src="sidemenu/home.gif" /></a><br />
+		<img src="sidemenu/products.gif" /><br />
+		<a href="rubbers.php"><img src="sidemenu/rubbers.gif" /></a><br />
+		<a href="blades.php"><img src="sidemenu/blades.gif" /></a><br />
+		<a href="balls.php"><img src="sidemenu/balls.gif" /></a><br />
+		<a href="accessories.php"><img src="sidemenu/accessories.gif" /></a><br />
+		<a href="usedblades.php"><img src="sidemenu/specials.gif" /></a><br />
+		<img src="sidemenu/midsection.gif" /><br />
+		<a href="howto.php"><img src="sidemenu/howto.gif" /></a><br />
+		<a href="contact.php"><img src="sidemenu/contact.gif" /></a><br />
+		<a href="index.php#payment"><img src="sidemenu/payment.gif" /></a><br />
+		<img src="sidemenu/bottomsection.gif" />
+	</div>
+	<br />
+	<center><a target="_blank" title="Ping Pong Videos and Reviews" href="http://www.tabletennisdb.com/"><img alt="Table Tennis Database" src="http://www.tabletennisdb.com/assets/table-tennis-images/ttdb-circle.gif" /></a></center>
+</div>
+
