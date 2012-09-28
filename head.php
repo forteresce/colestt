1f4a45597e1338fa557e08d270cf4226dab17276
Sheng <swang720@gmail.com>
2012-09-28 16:12:31 -0700

    Moved most of the header information into head.php.

---
+++ head.php
@@ -0,0 +1,13 @@
+<!-- This goes within the <head></head> tags. -->
+<title>Cole's Equipment Site</title>
+<meta http-equiv="content-type" content="text/html; charset=windows-1250">
+<link rel="stylesheet" type="text/css" href="css/cole.css" />
+<link rel="shortcut icon" href="favicon.ico" />
+
+<script src="js/nospam.js" type="text/javascript"></script>
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript"></script>
+<script type="text/javascript">
+    _uacct = "UA-993283-1";
+    urchinTracker();
+</script>
+
--- inverted.php
+++ inverted.php
@@ -1,19 +1,8 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 <head>
-	<title>Cole's Equipment Site</title>
-	<meta http-equiv="content-type" content="text/html; charset=windows-1250">
-	<link rel="stylesheet" type="text/css" href="css/cole.css" />
-	<link rel="shortcut icon" href="favicon.ico" />
-	
-	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
-</script>
-<script type="text/javascript">
-_uacct = "UA-993283-1";
-urchinTracker();
-</script>
-<script src="js/nospam.js" type="text/javascript"></script>
+    <?php include 'head.php' ?>
 </head>
 
 <body>
