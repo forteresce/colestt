381a495d7b5b5dffe1f072a234fea4dfbbe5a130
Sheng <swang720@gmail.com>
2012-09-28 14:37:36 -0700

    modified:   accessories.php 	modified:   balls.php 	modified:   blades.php 	modified:   compositeblades.php 	modified:   contact.php 	modified:   custom.php 	modified:   howto.php 	modified:   inverted.php 	modified:   pips.php 	modified:   rubbers.php 	modified:   topsheets-sponges.php 	modified:   usedblades.php 	modified:   woodblades.php

--- compositeblades.php
+++ compositeblades.php
@@ -22,24 +22,7 @@ urchinTracker();
 		<img src="header.gif" />
 </div> <!-- END OF header -->
 
-<div id="leftpane">
-	<div id="sidemenu"> <!-- Contains the menu on the left -->
-		<a href="index.php"><img src="sidemenu/home.gif" /></a><br />
-		<img src="sidemenu/products.gif" /><br />
-		<a href="rubbers.php"><img src="sidemenu/rubbers.gif" /></a><br />
-		<a href="blades.php"><img src="sidemenu/blades.gif" /></a><br />
-		<a href="balls.php"><img src="sidemenu/balls.gif" /></a><br />
-		<a href="accessories.php"><img src="sidemenu/accessories.gif" /></a><br />
-		<a href="usedblades.php"><img src="sidemenu/specials.gif" /></a><br />
-		<img src="sidemenu/midsection.gif" /><br />
-		<a href="howto.php"><img src="sidemenu/howto.gif" /></a><br />
-		<a href="contact.php"><img src="sidemenu/contact.gif" /></a><br />
-		<a href="index.php#payment"><img src="sidemenu/payment.gif" /></a><br />
-		<img src="sidemenu/bottomsection.gif" />
-	</div> <!-- END OF sidemenu -->
-	<br />
-	<center><a target="_blank" title="Ping Pong Videos and Reviews" href="http://www.tabletennisdb.com/"><img alt="Table Tennis Database" src="http://www.tabletennisdb.com/assets/table-tennis-images/ttdb-circle.gif" /></a></center>
-</div>
+<?php include 'leftpane.php'; ?>
 
 <div id="body"> <!-- Contains page content -->
 <!-- All page content STARTS here -->
