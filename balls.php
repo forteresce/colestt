463d3aa5a897a0756543381755ca339cc4c12ed3
Sheng <swang720@gmail.com>
2012-09-30 14:46:08 -0700

    Replaced <br /> in balls with <p> instead.

--- balls.php
+++ balls.php
@@ -11,8 +11,9 @@
     ?>
 
     <div id="body">
-        <img src="images/title/balls.gif"/><br />
-        <br />
+        <p>
+            <img src="images/title/balls.gif"/>
+        </p>
 
         <table class="product">
             <tr>
@@ -20,39 +21,38 @@
             </tr>
             <tr>
                 <td class="body">
-                
                     <span class="price">
                         Price: $3
                     </span>
-                    <br /><br />
-                    
-                    <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
-                        <table>
-                            <tr>
-                                <td><input type="hidden" name="on0" value="Color">Color</td>
-                                <td>
-                                    <select name="os0">
-                                        <option value="Orange">Orange</option>
-                                        <option value="White">White</option>
-                                    </select>
-                                </td>
-                            </tr>
-                        </table>
-                        <input type="image" src="https://www.paypal.com/en_US/i/btn/x-click-but22.gif" border="0" name="submit" alt="Make payments with PayPal - it's fast, free and secure!">
-                        <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
-                        <input type="hidden" name="add" value="1">
-                        <input type="hidden" name="cmd" value="_cart">
-                        <input type="hidden" name="business" value="cole.ely@gmail.com">
-                        <input type="hidden" name="item_name" value="Double Fish 3* Balls (Pack of 3)">
-                        <input type="hidden" name="amount" value="3">
-                        <input type="hidden" name="no_note" value="1">
-                        <input type="hidden" name="currency_code" value="USD">
-                        <input type="hidden" name="bn" value="PP-ShopCartBF">
-                    </form>
-                    <br />
-                    
-                    For me, this is the best 3 star ball. Although some people try both and prefer the DHS, I seem to get more repeat orders for these when customers try both.  They have a hard feel and are durable.  At $3 a box they're an incredible bargain. Can ship up to 8 boxes first class for $1. 
-                    Beyond that please don't choose the $1 option of shipping.  Minimum of 4 boxes if you're ordering just balls first class, so there's enough space created to paste on the label. 
+                    <p> 
+                        <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+                            <table>
+                                <tr>
+                                    <td>
+                                        <input type="hidden" name="on0" value="Color">Color
+                                    </td>
+                                    <td>
+                                        <select name="os0">
+                                            <option value="Orange">Orange</option>
+                                            <option value="White">White</option>
+                                        </select>
+                                    </td>
+                                </tr>
+                            </table>
+                            <input type="image" src="https://www.paypal.com/en_US/i/btn/x-click-but22.gif" border="0" name="submit" alt="Make payments with PayPal - it's fast, free and secure!">
+                            <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+                            <input type="hidden" name="add" value="1">
+                            <input type="hidden" name="cmd" value="_cart">
+                            <input type="hidden" name="business" value="cole.ely@gmail.com">
+                            <input type="hidden" name="item_name" value="Double Fish 3* Balls (Pack of 3)">
+                            <input type="hidden" name="amount" value="3">
+                            <input type="hidden" name="no_note" value="1">
+                            <input type="hidden" name="currency_code" value="USD">
+                            <input type="hidden" name="bn" value="PP-ShopCartBF">
+                        </form>
+                    </p>
+                    <p>For me, this is the best 3 star ball. Although some people try both and prefer the DHS, I seem to get more repeat orders for these when customers try both. They have a hard feel and are durable. At $3 a box they're an incredible bargain.</p>
+                    <p>I can ship up to 8 boxes first class for $1. Beyond that please don't choose the $1 option of shipping. Minimum of 4 boxes if you're ordering just balls first class, so there's enough space created to paste on the label.</p> 
                 </td>
                 <td class="image">
                     <img src="images/products/dfish3.jpg"/>
@@ -66,73 +66,82 @@
             </tr>
             <tr>
                 <td class="body">
-                    <span class="price">
-                        3* (Bag of 36) - $12<br />
-                        2* (Bag of 72) - $15<br />
-                    </span>
-                    <br /><br />
-
-                    Platinum 3 Star Balls
-                    <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
-                        <table>
-                            <tr>
-                                <td><input type="hidden" name="on0" value="Color">Color</td>
-                                <td>
-                                    <select name="os0">
-                                        <option value="Orange">Orange
-                                        <option value="White">White
-                                    </select>
-                                </td>
-                            </tr>
-                        </table>
-                        <input type="image" src="https://www.paypal.com/en_US/i/btn/x-click-but22.gif" border="0" name="submit" alt="Make payments with PayPal - it's fast, free and secure!">
-                        <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
-                        <input type="hidden" name="add" value="1">
-                        <input type="hidden" name="cmd" value="_cart">
-                        <input type="hidden" name="business" value="cole.ely@gmail.com">
-                        <input type="hidden" name="item_name" value="Gambler 3* Platinum Balls (Bag of 36)">
-                        <input type="hidden" name="amount" value="12">
-                        <input type="hidden" name="no_shipping" value="2">
-                        <input type="hidden" name="no_note" value="1">
-                        <input type="hidden" name="currency_code" value="USD">
-                        <input type="hidden" name="lc" value="US">
-                        <input type="hidden" name="bn" value="PP-ShopCartBF">
-                    </form>
-                    <br /><br />			
-
-                    Gold 2 Star Balls
-                    <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
-                        <table>
-                            <tr>
-                                <td><input type="hidden" name="on0" value="Color">Color</td>
-                                <td>
-                                    <select name="os0">
-                                        <option value="Orange">Orange
-                                        <option value="White">White
-                                    </select>
-                                </td>
-                            </tr>
-                        </table>
-                        <input type="image" src="https://www.paypal.com/en_US/i/btn/x-click-but22.gif" border="0" name="submit" alt="Make payments with PayPal - it's fast, free and secure!">
-                        <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
-                        <input type="hidden" name="add" value="1">
-                        <input type="hidden" name="cmd" value="_cart">
-                        <input type="hidden" name="business" value="cole.ely@gmail.com">
-                        <input type="hidden" name="item_name" value="Gambler 2* Gold Balls (Bag of 72)">
-                        <input type="hidden" name="amount" value="15">
-                        <input type="hidden" name="no_shipping" value="2">
-                        <input type="hidden" name="no_note" value="1">
-                        <input type="hidden" name="currency_code" value="USD">
-                        <input type="hidden" name="lc" value="US">
-                        <input type="hidden" name="bn" value="PP-ShopCartBF">
-                    </form>
-                    <br />
-
-                    Why pay for a pretty box you're going to throw away?  Because they're bagged and use a single colored stamp, they're half the price of comparable products, making them the best value in balls on the market.  The 3* are USATT
-                    approved tournament quality.  The 2* are great for service practice or multiball.  <br /><br /> BAGS OF BALLS CAN NOT BE SHIPPED FOR A DOLLAR, SO PLEASE DO NOT CHOOSE THE $1 OPTION WITH THEM. The change is made on the 2nd page of the cart,
-                    after you enter your info.<br /><br />
-                    Also, for international orders shipping must be quoted seperately with training balls because the bulk just makes shipping non-feasible in many cases.  The balls are light but the weight of the box it takes to ship them becomes substantial, although now that first class
-                    has tracking available I've been able to sell a lot more bags of balls overseas. 
+                    <p>
+                        <span class="price">
+                            3* (Bag of 36) - $15
+                            <br />
+                            2* (Bag of 72) - $12
+                        </span>
+                    </p> 
+                    <p>
+                        <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+                            Platinum 3 Star Balls
+                            <table>
+                                <tr>
+                                    <td>
+                                        <input type="hidden" name="on0" value="Color">Color
+                                    </td>
+                                    <td>
+                                        <select name="os0">
+                                            <option value="Orange">Orange
+                                            <option value="White">White
+                                        </select>
+                                    </td>
+                                </tr>
+                            </table>
+                            <input type="image" src="https://www.paypal.com/en_US/i/btn/x-click-but22.gif" border="0" name="submit" alt="Make payments with PayPal - it's fast, free and secure!">
+                            <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+                            <input type="hidden" name="add" value="1">
+                            <input type="hidden" name="cmd" value="_cart">
+                            <input type="hidden" name="business" value="cole.ely@gmail.com">
+                            <input type="hidden" name="item_name" value="Gambler 3* Platinum Balls (Bag of 36)">
+                            <input type="hidden" name="amount" value="15">
+                            <input type="hidden" name="no_shipping" value="2">
+                            <input type="hidden" name="no_note" value="1">
+                            <input type="hidden" name="currency_code" value="USD">
+                            <input type="hidden" name="lc" value="US">
+                            <input type="hidden" name="bn" value="PP-ShopCartBF">
+                        </form>
+                    </p>
+                    <p>
+                        <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+                            Gold 2 Star Balls
+                            <table>
+                                <tr>
+                                    <td>
+                                        <input type="hidden" name="on0" value="Color">Color
+                                    </td>
+                                    <td>
+                                        <select name="os0">
+                                            <option value="Orange">Orange
+                                            <option value="White">White
+                                        </select>
+                                    </td>
+                                </tr>
+                            </table>
+                            <input type="image" src="https://www.paypal.com/en_US/i/btn/x-click-but22.gif" border="0" name="submit" alt="Make payments with PayPal - it's fast, free and secure!">
+                            <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+                            <input type="hidden" name="add" value="1">
+                            <input type="hidden" name="cmd" value="_cart">
+                            <input type="hidden" name="business" value="cole.ely@gmail.com">
+                            <input type="hidden" name="item_name" value="Gambler 2* Gold Balls (Bag of 72)">
+                            <input type="hidden" name="amount" value="12">
+                            <input type="hidden" name="no_shipping" value="2">
+                            <input type="hidden" name="no_note" value="1">
+                            <input type="hidden" name="currency_code" value="USD">
+                            <input type="hidden" name="lc" value="US">
+                            <input type="hidden" name="bn" value="PP-ShopCartBF">
+                        </form>
+                    </p>
+                    <p>
+                        Why pay for a pretty box you're going to throw away? Because they're bagged and use a single colored stamp, they're half the price of comparable products, making them the best value in balls on the market. The 3 star are USATT approved tournament quality. The 2 star are great for service practice or multiball.
+                    </p>
+                    <p>
+                        <b>Bags of balls cannot be shipped for $1, so please don't choose the $1 option with them.</b> (The change is made on the 2nd page of the cart, after you enter your info.)
+                    </p>
+                    <p>
+                        Also, for international orders, shipping must be quoted separately with training balls because the bulk just makes shipping non-feasible in many cases. The balls are light but the weight of the box it takes to ship them becomes substantial, although now that first class has tracking available I've been able to sell a lot more bags of balls overseas.
+                    </p>
                 </td>
                 <td class="image">
                     <img src="images/products/gambler 3star.JPG" />
